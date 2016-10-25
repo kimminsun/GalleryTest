bd948770895088b3df6412392a570b506fd53566
Minsun Kim <minsun2767@naver.com>
2016-10-25 20:05:03 +0900

    #2 gallery

--- app/src/main/java/kr/hs/emrim/minsun2767/gallerytest/MainActivity.java
+++ app/src/main/java/kr/hs/emrim/minsun2767/gallerytest/MainActivity.java
@@ -1,13 +1,54 @@
 package kr.hs.emrim.minsun2767.gallerytest;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.VelocityTracker;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    Gallery gallery;
+    ImageView imgMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gallery=(Gallery)findViewById(R.id.gallery_1);
+        imgMain=(ImageView)findViewById(R.id.imgv_main);
+        MyGalleryAdapter adapter=new MyGalleryAdapter(this);
+        gallery.setAdapter(adapter);
+    }
+
+    public class MyGalleryAdapter extends BaseAdapter{
+        Context context;
+        int[] posterIDs={R.drawable.a1,R.drawable.a2,R.drawable.a3,R.drawable.a4,R.drawable.a5,R.drawable.a6,R.drawable.a7,R.drawable.a8,R.drawable.a9,R.drawable.a10};
+        public MyGalleryAdapter(Context context){
+            this.context=context;
+
+        }
+        public int getCount(){
+            return posterIDs.length;
+        }
+        public View getView(int position, View convertView, ViewGroup parent){
+            ImageView imgV=new ImageView(context);
+            imgV.setLayoutParams(new Gallery.LayoutParams(100,150));
+            imgV.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imgV.setPadding(5,5,5,5);
+            imgV.setImageResource(posterIDs[position]);
+            return imgV;
+        }
+        public long getItemId(int position){
+            return 0;
+
+        }
+        public Object getItem(int position){
+            return null;
+        }
+
     }
 }
